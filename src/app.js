dbf7f5ba3186e482f0165974d4ff3571e4e335f3
dianettee <dededianeta@gmail.com>
2021-12-18 10:30:19 +0100

    html, css and js update

---
+++ src/app.js
@@ -0,0 +1,5 @@
+let apiKey = "b5bbf587e13af95f0c77ca011722861f";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+console.log(apiKey);
+console.log(apiUrl);
