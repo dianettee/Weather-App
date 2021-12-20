ee627a0ac10a770d9dfadf896bc18fbecc953a04
dianettee <dededianeta@gmail.com>
2021-12-20 21:42:52 +0100

    Injected HTML forecast code into JS

--- src/app.js
+++ src/app.js
@@ -20,6 +20,32 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http://openweathermap.org/img/wn/09d@2x.png"
+                  alt=""
+                  width="30px"
+                />
+                <div class="weather-forecast-temp">
+                <span class="weather-forecast-temp-max"> 18°
+                </span>
+                <span class="weather-forecast-temp-min"> 12°
+                </span>
+                </div>
+                </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -87,3 +113,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 search("New York");
+displayForecast();
